7de614828c8ca9773cee0c846f2c58dc2046ed9b
Pablo <pablo.castro@witi.cl>
2019-08-22 00:21:43 -0400

    [REL]: Se agrega metodo para consumir soap en divide y multiply

--- src/divide/divide.service.ts
+++ src/divide/divide.service.ts
@@ -19,7 +19,31 @@ export class DivideService {
         400,
       );
     }
-    return await `El resultado de la division es: ${numbersDto.numberOne /
-      numbersDto.numberTwo}`;
+    const unirest = require('unirest');
+
+    return new Promise((resolve, reject) => {
+      unirest.post('http://www.dneonline.com/calculator.asmx')
+        .headers({
+          'cache-control': 'no-cache',
+          'Connection': 'keep-alive',
+          'Content-Length': '289',
+          'Accept-Encoding': 'gzip, deflate',
+          'Host': 'www.dneonline.com',
+          'Postman-Token': 'adf96ff7-01ec-4e8b-9258-1a34a1f8b2b2,c8aa09cd-96a2-45d7-be66-7a1cdc1d743d',
+          'Cache-Control': 'no-cache',
+          'Accept': '*/*',
+          'User-Agent': 'PostmanRuntime/7.15.2',
+          'Content-Type': 'text/xml',
+        })
+        // tslint:disable-next-line: max-line-length
+        .send(`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tem:Divide>\n         <tem:intA>${numbersDto.numberOne}</tem:intA>\n         <tem:intB>${numbersDto.numberTwo}</tem:intB>\n      </tem:Divide>\n   </soapenv:Body>\n</soapenv:Envelope>`)
+        // tslint:disable-next-line: only-arrow-functions
+        .end(function(response) {
+          const convert = require('xml-js');
+          if (response.error) { return reject(Error); }
+          const xmlToJson = convert.xml2json(response.body, { compact: true, spaces: 4 });
+          return resolve(xmlToJson);
+        });
+    });
   }
 }
--- src/multiply/multiply.service.ts
+++ src/multiply/multiply.service.ts
@@ -4,10 +4,31 @@ import { NumbersDto } from '../dto/numers.dto';
 @Injectable()
 export class MultiplyService {
   async multiplyTwoNumbers(numbersDto: NumbersDto): Promise<any> {
-    return await `El producto de los numeros es: ${(numbersDto.numberOne ===
-    undefined
-      ? 0
-      : numbersDto.numberOne) *
-      (numbersDto.numberTwo === undefined ? 0 : numbersDto.numberTwo)}`;
+    const unirest = require('unirest');
+
+    return new Promise((resolve, reject) => {
+      unirest.post('http://www.dneonline.com/calculator.asmx')
+        .headers({
+          'cache-control': 'no-cache',
+          'Connection': 'keep-alive',
+          'Content-Length': '296',
+          'Accept-Encoding': 'gzip, deflate',
+          'Host': 'www.dneonline.com',
+          'Postman-Token': 'adf96ff7-01ec-4e8b-9258-1a34a1f8b2b2,c8aa09cd-96a2-45d7-be66-7a1cdc1d743d',
+          'Cache-Control': 'no-cache',
+          'Accept': '*/*',
+          'User-Agent': 'PostmanRuntime/7.15.2',
+          'Content-Type': 'text/xml',
+        })
+        // tslint:disable-next-line: max-line-length
+        .send(`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tem:Multiply>\n         <tem:intA>${numbersDto.numberOne}</tem:intA>\n         <tem:intB>${numbersDto.numberTwo}</tem:intB>\n      </tem:Multiply>\n   </soapenv:Body>\n   </soapenv:Envelope`)
+        // tslint:disable-next-line: only-arrow-functions
+        .end(function(response) {
+          const convert = require('xml-js');
+          if (response.error) { return reject(Error); }
+          const xmlToJson = convert.xml2json(response.body, { compact: true, spaces: 4 });
+          return resolve(xmlToJson);
+        });
+    });
   }
 }
