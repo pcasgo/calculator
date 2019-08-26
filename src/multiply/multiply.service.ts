2cb58f1a4ea40d5cb46e02efc3e3085a552dc2aa
Pablo <pablo.castro@witi.cl>
2019-08-26 02:32:34 -0400

    [FIX]: Se corrige largo para llamada de servicio en multiplicacion

--- src/multiply/multiply.service.ts
+++ src/multiply/multiply.service.ts
@@ -11,17 +11,17 @@ export class MultiplyService {
         .headers({
           'cache-control': 'no-cache',
           'Connection': 'keep-alive',
-          'Content-Length': '296',
+          'Content-Length': '293',
           'Accept-Encoding': 'gzip, deflate',
           'Host': 'www.dneonline.com',
-          'Postman-Token': 'adf96ff7-01ec-4e8b-9258-1a34a1f8b2b2,c8aa09cd-96a2-45d7-be66-7a1cdc1d743d',
+          'Postman-Token': 'dfea8a8a-a980-4f00-8ded-7ab453bd7f62,57a1f796-4122-40cb-9a82-48ae389a1b81',
           'Cache-Control': 'no-cache',
           'Accept': '*/*',
           'User-Agent': 'PostmanRuntime/7.15.2',
           'Content-Type': 'text/xml',
         })
         // tslint:disable-next-line: max-line-length
-        .send(`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tem:Multiply>\n         <tem:intA>${numbersDto.numberOne}</tem:intA>\n         <tem:intB>${numbersDto.numberTwo}</tem:intB>\n      </tem:Multiply>\n   </soapenv:Body>\n   </soapenv:Envelope`)
+        .send(`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tem:Multiply>\n         <tem:intA>${numbersDto.numberOne}</tem:intA>\n         <tem:intB>${numbersDto.numberTwo}</tem:intB>\n      </tem:Multiply>\n   </soapenv:Body>\n</soapenv:Envelope>`)
         // tslint:disable-next-line: only-arrow-functions
         .end(function(response) {
           const convert = require('xml-js');
